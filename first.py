1187bab9aadfbe040db8dd1e749a075582e0cbc1
VedanshS <74900340+VedanshS@users.noreply.github.com>
2021-09-06 18:38:41 +0530

    swapped

--- first.py
+++ first.py
@@ -1,33 +1,18 @@
-import random
-
-print("Number guessing game")
-
-num = random.randint(1, 7)
-chances = 5
-
-print("Guess a number (Between 1 and 7):")
-
-while chances > 1:
-
-    guess = int(input("Enter your guess:- "))
-
-    if guess == num:
-        print("Congratulation YOU WON!!!")
-        
-    elif guess < num:
-        print("Your guess was too low: Guess a number higher than", guess)
-
-    else:
-        print("Your guess was too high: Guess a number lower than", guess)
-
-    chances -= 1
-
-while chances == 1:
-    if guess == num:
-        print("Congratulation YOU WON!!!")
-    else : 
-        chances -= 1
+def swapFileData():
+    file1 = input("file1:- ")
+    file2 = input("file2:- ")
 
 
-if chances == 0:
-    print("YOU LOSE ! The number was", num)
+    with open(file1, 'r') as a:
+        data_a = a.read()
+
+    with open(file2, 'r') as b:
+        data_b = b.read()
+
+    with open(file1, 'w') as a:
+        a.write(data_b)
+
+    with open(file2, 'w') as b:
+        b.write(data_a)
+
+swapFileData()
